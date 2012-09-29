66ef676442c84675aebfaaa390025dba6b4d7f4e
Nieve <nievegoor@gmail.com>
2012-09-29 12:14:18 +0100

    first rule

--- spec/gol_spec.rb
+++ spec/gol_spec.rb
@@ -1,18 +1,7 @@
-describe "Any live cell with fewer than two live neighbours dies, as if caused by under-population." do
-	x = 4
-	y = 4
-	cell=Cell.new(x,y)
-	lonely = Cell.new(44,44)
-	cell_neighbour=Cell.new(x+1,y)
-	cell_array=[cell,cell_neighbour,lonely]
-
-	gol = Gol.new cell_array
-	gol.play
-
-	cell_array.should_be empty
-end
+require 'rspec/expectations'
 
 class Cell
+	attr_accessor :x, :y
  	def initialize(x,y)
  		@x=x
  		@y=y
@@ -27,15 +16,31 @@ class Gol
 
 	def play
 		should_die=[]
-		cell_array.each do |current_cell|
+		@cell_array.each do |current_cell|
 			neighbour_count = 0
-			cell_array.each do |other_cells|
-				if other_cells != current_cell && (other_cells.x-current_cell.x).abs<=1 && (other_cells.y-current_cell.y).abs<=1 neighbour_count += 1
+			@cell_array.each do |other_cells|
+				neighbour_count += 1 if (other_cells != current_cell && (other_cells.x-current_cell.x).abs<=1 && (other_cells.y-current_cell.y).abs<=1)
 			end
-			if neighbour_count < 2 should_die << current_cell
+			should_die << current_cell if (neighbour_count < 2)
 		end
-	cell_array-=should_die
+		@cell_array-=should_die
+		@cell_array
 	end
+end
 
 
+describe "Any live cell with fewer than two live neighbours dies, as if caused by under-population." do
+	x = 4
+	y = 4
+	cell=Cell.new(x,y)
+	lonely = Cell.new(44,44)
+	cell_neighbour=Cell.new(x+1,y)
+	cell_array=[cell,cell_neighbour,lonely]
 
+	gol = Gol.new cell_array
+	cell_array = gol.play
+
+	it "should be_empty" do
+		cell_array.should be_empty
+	end
+end
